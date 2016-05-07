6248d9efeb7813cf61a19c6d41be3f82bbf24d8e
lavanya <lavanya@lavanya-PC>
2016-05-07 11:59:10 +0530

    new

--- app/src/emHome/emHomeCtrl.js
+++ app/src/emHome/emHomeCtrl.js
@@ -3,13 +3,13 @@
   angular
        .module('emHome')
        .controller('emHomeCtrl', [
-          'emHomeService', '$mdSidenav', '$mdBottomSheet', '$log','$scope', '$q' ,'$state', '$stateParams',
+           '$mdSidenav', '$mdBottomSheet', '$log','$scope', '$q' ,'$state', '$stateParams',
           EmHomeCtrl
        ]).directive( 'appHeader', function() {
   return {
      restrict: 'E',
      
-       templateUrl: "/src/navBar/view/navBar.html"
+       templateUrl: "/src/navbar/view/navBar.html"
   };
 }).directive( 'appFooter', function() {
   return {
@@ -19,7 +19,7 @@
   };
 });
 
-  function  EmHomeCtrl( EmHomeService, $mdSidenav, $mdBottomSheet, $log,$scope, $q, $state, $stateParams) {
+  function  EmHomeCtrl( $mdSidenav, $mdBottomSheet, $log,$scope, $q, $state, $stateParams) {
     var self = this;
 
     self.load         = load;
@@ -29,25 +29,12 @@
      * Load function to initialize the current template with default services
      */
     function load(){
-    	$scope.searchParams = $stateParams;
-    	$log.info("Inside load of EmHomeCtrl "+JSON.stringify($stateParams));
-    	getEmHome($scope.searchParams);
+   
+    	$log.info("Inside load of EmHomeCtrl ");
+
     }
 
-    function getEmHome(params){
-    	$log.debug("In getEmHome params  : "+JSON.stringify(params));
-		
-    	try{
-    		emHomeService.getEmHome(params)
-    		.then(function(response){
-    			$log.debug("In EmHomeCtrl  : "+JSON.stringify(response));
-    			$scope.emHomeData = response[0];
-    		     $log.debug("$scope.emHomeData : "+JSON.stringify($scope.emHomeData));
-    		})
-    	}catch(e){
-    			$log.debug(e);
-    		} 
-    } 
+    
       
    
 
